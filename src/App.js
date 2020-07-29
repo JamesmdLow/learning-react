af99996efafe41c77e3d0ca8c1a48117f6359e64
James Low <jamesmdlow@gmail.com>
2020-07-29 14:13:02 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+               Challenge accepted
+            </div>
+        );
+    }
+}
+
+export default App;
